dc049b6108f32f70333593c2b8a9d058817fde36
Adriano Zurita Martinez <adrianozm07@gmail.com>
2022-10-25 12:49:09 +0200

    Cuarto commit

--- azm_exameniaw2022.php
+++ azm_exameniaw2022.php
@@ -6,5 +6,6 @@
   <h1><?php echo "Adriano Zurita Martinez" ?></h1>
   <h1><?php echo "25/10/2022" ?></h1>
   <h1><?php echo "este es mi repositorio azm_exameniaw2022.php" ?></h1>
+  <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.m.wikipedia.org%2Fwiki%2FArchivo%3AGit-logo.svg&psig=AOvVaw26GLVc4XkMThPwur2Pkada&ust=1666781222061000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCLDC84aa-_oCFQAAAAAdAAAAABAE">
  </body>
 </html>
